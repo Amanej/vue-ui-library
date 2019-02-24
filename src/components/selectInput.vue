024af84ddb29cdb84e7b52a95adc88884df3346f
Aman <aman.mender@tegningtil3d.no>
2019-02-24 13:07:51 +0100

    Working dynamic custom select

--- src/components/selectInput.vue
+++ src/components/selectInput.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="selectInput" :class="{open: open}">
         <div class="selected"> <!-- Selected container -->
-            <span>Selected</span>
+            <span v-if="!selectedOpt">Select an option</span>
+            <span v-if="selectedOpt">{{selectedOpt.label}}</span>
             <label for="toggleSelect">
                 <i>+</i><!-- icon -->
                 <input type="checkbox" v-model="open" id="toggleSelect" name="toggleSelect" />
             </label>
         </div>
         <ul class="options"> <!-- Options -->
-            <li>Option 1</li>
-            <li>Option 2</li>
-            <li>Option 3</li>
-            <li>Option 4</li>        
+            <li v-for="option in options" :key="option.key" @click="setSelectedOption(option)">
+                {{option.label}}
+            </li>
         </ul>
     </div>
 </template>
@@ -19,9 +19,43 @@
 <script>
 export default {
     name: 'selectInput',
+    computed: {
+        selectedOption() {
+            return this.options.find(o => o.selected)
+        }
+    },
+    methods: {
+        setSelectedOption(opt) {
+            this.selectedOpt = opt;
+            this.open = false;
+        }
+    },
     data() {
         return {
-            open: false
+            open: false,
+            selectedOpt: null,
+            options: [
+                {
+                    label: 'Option 1',
+                    key: 'opt1',
+                    selected: false
+                },
+                {
+                    label: 'Option 2',
+                    key: 'opt2',
+                    selected: false
+                },
+                {
+                    label: 'Option 3',
+                    key: 'opt3',
+                    selected: false
+                },
+                {
+                    label: 'Option 4',
+                    key: 'opt4',
+                    selected: false
+                }
+            ]
         }
     }
 }
@@ -58,21 +92,25 @@ div.selectInput {
     }
     ul.options {
         list-style-type:    none;
-        padding: 0;
-        width: 100%;
-        flex-direction: column;
+        padding:            0;
+        width:              100%;
+        flex-direction:     column;
         text-align:         left;
         border:             1px solid lightgrey;
-        margin: 0;
-        display: none;
+        margin:             0;
+        display:            none;
         li {
-            padding: 5px 10px;
-            border-bottom: 1px solid lightgrey;
+            padding:                5px 10px;
+            border-bottom:          1px solid lightgrey;
+            cursor:                 pointer;
             &:first-child {
-                border-top: none;                
+                border-top:         none;                
             }
             &:last-child {
-                border-bottom: none;
+                border-bottom:      none;
+            }
+            &:hover, &.selected {
+                background:         lightgrey;
             }
         }
     }
